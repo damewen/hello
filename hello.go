6eb90570ac98459602018a2f9399932d09e14c5b
Yoann Rigollé <yoann@macbook-de-yoann-rigolle.home>
2016-02-01 23:44:43 +0100

    script with awk package : affichage seconde colonne d'un fichier

--- hello.go
+++ hello.go
@@ -7,13 +7,15 @@ import (
 )
 
 func main() {
-	fmt.Printf("search for \"yoann\" \n")
+	fmt.Printf("affichage de la colonne 2 \n")
 	arg := os.Args[1]
 	fi, err := os.Open(arg)
 	if err != nil {
         panic(err)
     }
 	s := awk.NewScript()
-	s.AppendStmt(awk.Auto("yoann"), nil)
+	//s.AppendStmt(awk.Auto("yoann"), nil)
+	s.SetFS(";")
+	s.AppendStmt(nil, func(s *awk.Script) { s.Println(s.F(2)) })
 	s.Run(fi)
 }
